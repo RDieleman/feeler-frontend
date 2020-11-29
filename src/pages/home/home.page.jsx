14a6945b97490730a30dbe8a5c3bc47647a8a7f8
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-29 14:33:02 +0100

    added homepage

---
+++ src/pages/home/home.page.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import "./home.page.css";
+
+class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <h1>Home Page</h1>
+        );
+    }
+}
+
+export default HomePage;
