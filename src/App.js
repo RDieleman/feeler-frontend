f769acab1892bcf3149fef10676a20ef71b173b5
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-29 14:36:17 +0100

    added router and homepage redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
+import HomePage from "./pages/home/home.page";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Feeler application</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
