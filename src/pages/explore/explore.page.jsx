f9220cd84266ee0cc061058166fe4baa36039a5c
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-13 03:41:50 +0100

    Fixed explore mood bug

--- src/pages/explore/explore.page.jsx
+++ src/pages/explore/explore.page.jsx
@@ -10,7 +10,7 @@ class ExplorePage extends Component {
         super(props);
 
         this.state = {
-            selectedMood: null
+            selectedMood: this.props.moods[0]
         };
     };
 
