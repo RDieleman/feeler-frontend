a31a3bc8da7ee4dabbcbd3fa29d5d8ba6af0b37f
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 16:12:20 +0100

    Fixed test workspace location bug

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "================================"
 echo "======== Testing Code =========="
 echo "================================"
 
-WORKSPACE=/home/ruben/jenkins/jenkins_home/workspace/feeler-backend
+WORKSPACE=/home/ruben/jenkins/jenkins_home/workspace/feeler-frontend
 
 docker run --rm  -v  $WORKSPACE/code:/app -w /app feeler-frontend:latest "$@"
