51670546a4749a985fcc49cc27ba3ccf0775280b
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 17:24:56 +0100

    Attempted fix

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,4 +6,4 @@ echo "================================"
 
 WORKSPACE=/home/ruben/jenkins/jenkins_home/workspace/feeler-frontend
 
-docker run --rm  -v  $WORKSPACE/code:/app -w /app feeler-frontend:latest "$@"
+docker run --rm -v $WORKSPACE/code:/app -w /app feeler-frontend:latest "$@"
