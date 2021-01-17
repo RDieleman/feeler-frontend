45763c7bf178ad4ea76d9fd4b831c14fd3d2fdf9
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 15:52:28 +0100

    Fixed copy statement

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the new jar to the build location
-cp -f code/ jenkins/build/code/
+cp -rf /code/ /jenkins/build/code/
 
 echo "======================================="
 echo "======== Building Docker Image ========"
