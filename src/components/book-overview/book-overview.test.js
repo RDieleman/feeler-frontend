6628dcb9090bb254adeff09195272cd9f02375c2
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 13:47:12 +0100

    Implemented enzyme for testing

---
+++ src/components/book-overview/book-overview.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import {BookOverview} from "./book-overview.component";
+import {Book} from "../../models/book";
+
+const books = [
+    new Book(
+        "test title 1",
+        "test overview 1",
+        "12345",
+        "test image url 1"
+    ),
+    new Book(
+        "test title 2",
+        "test overview 2",
+        "12345",
+        "test image url 2"
+    )
+]
+
+it("Expect to render Book Overview component", () =>{
+    expect(shallow(
+        <BookOverview
+            handleOnClick={() => console.log("Book clicked")}
+            books={books}
+    />).length).toEqual(1);
+});
