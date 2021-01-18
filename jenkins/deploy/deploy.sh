a7e5a8e8f76a4b6a0b5708506b1bde8220dd31ff
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-18 02:15:04 +0100

    Bug fix attempt

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,4 +4,4 @@ echo "================================"
 echo "======= Deploying Code ========="
 echo "================================"
 
-docker-compose -f ./jenkins/deploy/docker-compose.yml up -d
+docker-compose -f ./jenkins/deploy/docker-compose.yml up -d --no-cache
